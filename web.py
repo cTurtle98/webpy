016618fa90e73b5cbfab170a06d9585f9b2cd537
cTurtle98 <cturtle98@98seaturtles.net>
2018-11-29 12:10:14 -0800

    update css

--- web.py
+++ web.py
@@ -2,12 +2,12 @@
 Ciaran Farley
 webapp to host www.cturtle98.com
 """
+import os
 from flask import Flask, render_template, send_from_directory
-import sys
 
 
 
-app = Flask(__name__, template_folder='templates/', static_url_path='/home/ciaran/webpy/include/')
+app = Flask(__name__, template_folder='templates/')
 
 @app.route('/')
 def index() :
@@ -18,13 +18,15 @@ def index() :
 		subtitle = "The projects of Ciaran farley",
 		content = """webcome to my projects, look around, maybe you will find something interesting"""
 		)
-
+"""
 @app.route('/main.css')
 def main_css():
 	f = open('include/main.css')
 	css = f.read()
 	f.close()
 	return css
+"""
+
 
 if __name__ == '__main__' :
     app.run(host='::', port=8088)
