a8ed21285344a70b2e3b3f957b1877d6d2811a32
cTurtle98 <ciaran@cturtle98.com>
2019-05-27 23:48:16 -0700

    rename to better reflect the data type

--- web.py
+++ web.py
@@ -3,19 +3,32 @@ Ciaran Farley
 webapp to host www.cturtle98.com
 """
 import os
+import json
 from flask import Flask, render_template, send_from_directory
 
 
 
 app = Flask(__name__, template_folder='html/templates/', static_folder = 'html/static')
 
+
+
 @app.route('/')
 def index() :
-	return render_template('home.html',)
+	return render_template('home.jinja2',)
 
 @app.route('/ham/')
 def ham() :
-	return render_template('ham.html')
+
+  my_equipment = {}
+
+  try:
+    f = open("data/ham/my_equipment.json")
+    my_equipment = json.load(f)
+    f.close
+  except:
+    print("couldn't find my_equipment.json")
+
+	return render_template('ham.jinja2', my_equipment)
 
 if __name__ == '__main__' :
     app.run(host='::', port=80)
